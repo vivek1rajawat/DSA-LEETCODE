9ac0e430ff2717c41e0f460c8d4c8632eb0ed009
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-06-28 15:24:09 +0530

    Time: 68 ms (5.07%), Space: 59.1 MB (53.94%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,14 +11,16 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let seenNodes = new Set();
-    let curr = head;
-    while(curr){
-        if(seenNodes.has(curr)){
-            return true;
+    if(!head) return false;
+    let slow = head;
+    let fast = head.next;
+    while(slow!=fast){
+        if(fast === null || fast.next === null){
+            return false;
         }
-        seenNodes.add(curr);
-        curr = curr.next;
+        slow = slow.next;
+        fast = fast.next.next
+        
     }
-    return false;
+    return true;
 };
