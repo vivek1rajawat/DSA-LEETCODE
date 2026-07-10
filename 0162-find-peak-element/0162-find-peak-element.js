35cc1d7ee0e3e9e92a0afb6207015b8bf9e82f6c
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-07-10 10:58:07 +0530

    Time: 0 ms (100%), Space: 54.3 MB (16.07%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let l = 0;
+    let r = nums.length-1;
+    while(l<r){
+        let m = Math.floor((l+r)/2);
+        if(nums[m]<nums[m+1]){
+            l = m+1;
+        }
+        else {
+            r = m;
+        }
+    }
+    return r;
+};
