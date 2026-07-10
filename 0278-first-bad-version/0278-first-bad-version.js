754294013a46024d1c1c94ecfbdab6db1870b638
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-07-10 10:21:22 +0530

    Time: 53 ms (6.36%), Space: 55.1 MB (5.67%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 1;
+        let r = n;
+        while(l<=r){
+            let m = Math.floor((l+r)/2);
+            if(l==r){
+                return r;
+            }
+            else if(isBadVersion(m)==true){
+                r = m;
+            }
+            else {
+                l = m+1;
+            }
+        }
+        
+    };
+};
