0f19e50e60752d27a55d18427c5eded496a36bd1
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-07-24 19:42:28 +0530

    Time: 0 ms (100%), Space: 53.6 MB (70.3%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let ans = [];
+    function traversal(curr){
+        if(!curr) return;
+        
+        traversal(curr.left);
+        ans.push(curr.val);
+        traversal(curr.right)
+    }
+    traversal(root);
+    return ans;
+};
