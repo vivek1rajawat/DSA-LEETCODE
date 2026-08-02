e5f4490afa15b9f4a035e54410dc753ba523cbd5
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-08-02 21:26:06 +0530

    Time: 2 ms (42.53%), Space: 55.3 MB (50.96%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+
+    let set = new Set(nums1);
+
+    let ans = new Set();
+
+    for (let i = 0; i < nums2.length; i++) {
+
+        if (set.has(nums2[i])) {
+            ans.add(nums2[i]);
+        }
+    }
+
+    return [...ans];
+};
