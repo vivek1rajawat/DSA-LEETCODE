da3c34464abf1c5221b06c4448fe426c5c60b9bc
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-09-01 09:18:56 +0530

    Time: 0 ms (100%), Space: 58.6 MB (19.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,10 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     let x = 0;
-    for(i = 0;i<nums.length;i++){
-        if(nums[i] > nums[x]){
+
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i]!=nums[x]){
             x = x+1;
-            nums[x] = nums[i];
+            nums[x] = nums[i]
         }
     }
     return x+1;
