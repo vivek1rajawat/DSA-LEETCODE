f30ccd4716089f0f0a863ad0fab2b586b114c039
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-09-11 09:51:33 +0530

    Time: 3 ms (74.91%), Space: 56.2 MB (61.56%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -3,20 +3,20 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    let stack = [];
+    let level = 0;
     let ans = "";
     for(let i = 0; i<s.length; i++){
         if(s[i]=="("){
-            stack.push(s[i]);
-            if(stack.length>1){
+            level++;
+            if(level>1){
                 ans = ans+s[i];
             }
 
         } else {
-            if(stack.length>1){
+            if(level>1){
                 ans = ans+s[i];
             }
-            stack.pop();
+            level--;
             
         }
     }
