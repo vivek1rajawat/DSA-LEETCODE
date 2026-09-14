25d05a40feee318b49e5ced9929975dfae639ec3
Vivek Rajawat <vivekrajawat2005@gmail.com>
2026-09-14 15:35:48 +0530

    Time: 0 ms (100%), Space: 54 MB (33.43%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -14,10 +14,10 @@ var inorderTraversal = function(root) {
     let ans = [];
     function traversal(curr){
         if(!curr) return;
-        
         traversal(curr.left);
         ans.push(curr.val);
-        traversal(curr.right)
+        traversal(curr.right);
+        
     }
     traversal(root);
     return ans;
